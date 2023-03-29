3b764ca11bff03fa7ff51e62f2d90de007bc1377
Thales <thales.souz@outlook.com>
2023-03-29 19:11:38 -0300

    test: test for method "getAll" on user Model

--- server/src/Tests/User.test.ts
+++ server/src/Tests/User.test.ts
@@ -1,7 +1,18 @@
-import { describe, it } from 'vitest'
+import sinon from 'sinon'
+import { describe, it, expect } from 'vitest'
+import { UserModel } from '../Database/Models'
+import { userGetResponse } from './Mocks/User.mock'
 
-describe('Tests for User and his use cases', () => {
-  it('should be able to create a new user with a valid email', () => {
+describe('Tests for User Domain and his sucess cases', () => {
+  it('Should be able to create a new user with a valid body', () => {
+    sinon.stub(new UserModel(), 'getAll').resolves(userGetResponse)
+    const newUser = {
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'johndoe@email.com',
+      password: 'password'
+    }
 
+    expect(newUser.first_name).toBe('John')
   })
 })
