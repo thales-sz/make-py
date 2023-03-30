59966434300cd1d053929a977e3d1616296eb732
Thales <thales.souz@outlook.com>
2023-03-30 14:34:37 -0300

    test(fix): change mocks and tests return

--- server/src/Tests/User.test.ts
+++ server/src/Tests/User.test.ts
@@ -3,25 +3,9 @@ import chai from 'chai'
 import chaiHttp from 'chai-http'
 import { describe, it, expect, afterEach } from 'vitest'
 import { UserModel } from '../Database/Models'
-import { userGetResponse } from './Mocks/User.mock'
+import * as Mock from './Mocks/User.mock'
 
 import { app } from '../app'
-
-const newUserResponse = {
-  id: '7845123123123',
-  first_name: 'John',
-  last_name: 'Doe',
-  email: 'johndoes@email.com',
-  role: 'USER',
-  password: 'password'
-}
-
-const newUser = {
-  first_name: 'John',
-  last_name: 'Doe',
-  email: 'johndoes@email.com',
-  password: 'password'
-}
 
 chai.use(chaiHttp)
 
@@ -29,7 +13,7 @@ describe('Tests for User Domain and his sucess cases', () => {
   afterEach(() => { sinon.restore() })
 
   it('Should return a list with all users from DB', async () => {
-    sinon.stub(UserModel.prototype, 'getAll').resolves(userGetResponse)
+    sinon.stub(UserModel.prototype, 'getAll').resolves(Mock.getAll.response)
 
     const { status, ok, body } = await chai.request(app).get('/user')
 
@@ -40,17 +24,16 @@ describe('Tests for User Domain and his sucess cases', () => {
   })
 
   it('Should be able to create a new user with valid credentials', async () => {
-    sinon.stub(UserModel.prototype, 'create').resolves(newUserResponse)
+    sinon.stub(UserModel.prototype, 'create').resolves(Mock.create.response)
 
     const { status, ok, body } = await chai
       .request(app)
       .post('/user/signup')
-      .send(newUser)
+      .send(Mock.create.send)
 
-    console.log(body)
     expect(status).toBe(201)
     expect(ok).toBeTruthy()
     expect(body).to.haveOwnProperty('token')
-    expect(newUser.email).to.equal(newUserResponse.email)
+    expect(body).to.haveOwnProperty('result')
   })
 })
