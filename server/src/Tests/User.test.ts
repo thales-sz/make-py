993a80dbc99f0b3a38aceadbd5f76d0726aeb638
Thales <thales.souz@outlook.com>
2023-03-30 14:09:38 -0300

    fix(test): test do not acess the real DB anymore

--- server/src/Tests/User.test.ts
+++ server/src/Tests/User.test.ts
@@ -8,10 +8,10 @@ import { userGetResponse } from './Mocks/User.mock'
 import { app } from '../app'
 
 const newUserResponse = {
-  id: '7845123',
+  id: '7845123123123',
   first_name: 'John',
   last_name: 'Doe',
-  email: 'johndoe@email.com',
+  email: 'johndoes@email.com',
   role: 'USER',
   password: 'password'
 }
@@ -19,7 +19,7 @@ const newUserResponse = {
 const newUser = {
   first_name: 'John',
   last_name: 'Doe',
-  email: 'johndoe@email.com',
+  email: 'johndoes@email.com',
   password: 'password'
 }
 
@@ -29,25 +29,28 @@ describe('Tests for User Domain and his sucess cases', () => {
   afterEach(() => { sinon.restore() })
 
   it('Should return a list with all users from DB', async () => {
-    sinon.stub(new UserModel(), 'getAll').resolves(userGetResponse)
+    sinon.stub(UserModel.prototype, 'getAll').resolves(userGetResponse)
 
     const { status, ok, body } = await chai.request(app).get('/user')
 
     expect(status).toBe(200)
     expect(ok).toBeTruthy()
-    expect(body[0]).toHaveProperty('id')
+    expect(body).toBeTypeOf('object')
+    expect(body[0]).to.haveOwnProperty('id')
   })
 
-  it('Should create a new User in the DB', async () => {
-    sinon.stub(new UserModel(), 'create').resolves(newUserResponse)
+  it('Should be able to create a new user with valid credentials', async () => {
+    sinon.stub(UserModel.prototype, 'create').resolves(newUserResponse)
 
     const { status, ok, body } = await chai
       .request(app)
       .post('/user/signup')
       .send(newUser)
 
+    console.log(body)
     expect(status).toBe(201)
     expect(ok).toBeTruthy()
-    expect(body[0]).toHaveProperty('id')
+    expect(body).to.haveOwnProperty('token')
+    expect(newUser.email).to.equal(newUserResponse.email)
   })
 })
