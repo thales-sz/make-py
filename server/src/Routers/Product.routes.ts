a69c8ed238bd7264ddf095ee5c9905ded0042d12
Thales <thales.souz@outlook.com>
2023-03-31 14:26:47 -0300

    feat: Router GET /products done

--- server/src/Routers/Product.routes.ts
+++ server/src/Routers/Product.routes.ts
@@ -4,6 +4,6 @@ import { ProductController } from '../Controllers'
 const router = express.Router()
 
 router.get('/', async (req, res, next) => await new ProductController(req, res, next).getAll())
-router.get('/:id', async (req, res, next) => await new ProductController(req, res, next).getById())
+// router.get('/:id', async (req, res, next) => await new ProductController(req, res, next).getById())
 
 export default router
