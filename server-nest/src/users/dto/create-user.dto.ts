b460124ac3c76587c12929ca1f1af957f90bfd66
Thales <thales.souz@outlook.com>
2023-04-12 08:27:42 -0300

    chore: install class-validator and class-transformer to validate dtos

--- server-nest/src/users/dto/create-user.dto.ts
+++ server-nest/src/users/dto/create-user.dto.ts
@@ -1,3 +1,11 @@
 export class CreateUserDto {
+  id: string;
   firstName: string;
+  lastName: string;
+  email: string;
+  role: Role;
+  password: string;
+  sale: Sale;
+  phoneNumber: string;
+  address: Address;
 }
