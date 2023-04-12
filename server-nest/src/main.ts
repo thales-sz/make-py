dd751c58487111ad2e1286ba35e20e94b7a6cd24
Thales <thales.souz@outlook.com>
2023-04-12 08:10:54 -0300

    build: message on server up

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(PORT)
+    .then(() => {
+      console.log(`App running on PORT: ${PORT}`);
+    })
+    .catch((err) => console.error(`Error: ${err}`));
 }
 bootstrap();
