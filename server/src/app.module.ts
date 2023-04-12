639e8c114f801cdb79b24cf656a024dd319c9fdf
Thales <thales.souz@outlook.com>
2023-04-12 16:03:37 -0300

    feat: auth module created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, ProductsModule, SalesModule],
+  imports: [UsersModule, PrismaModule, ProductsModule, SalesModule, AuthModule],
   controllers: [],
   providers: [],
 })
