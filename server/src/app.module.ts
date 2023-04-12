7aada857aed40779ad92a3d6a77ddee0e3e4c454
Thales <thales.souz@outlook.com>
2023-04-12 13:35:08 -0300

    feat: products structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, ProductsModule],
   controllers: [],
   providers: [],
 })
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   ConflictException,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,31 +18,43 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const oldUser = this.usersService.findOneByEmail(createUserDto.email);
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.findOneByEmail(createUserDto.email);
 
-    if (oldUser) throw new ConflictException(`This email is already in use`);
+    if (user) throw new ConflictException(`This email is already in use`);
 
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) throw new NotFoundException(`User with id: '${id}' not found`);
+
+    return user;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) throw new NotFoundException(`User with id: '${id}' not found`);
+
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) throw new NotFoundException(`User with id: '${id}' not found`);
+
     return this.usersService.remove(id);
   }
 }
