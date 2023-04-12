cb35c28f287de4ca466cb2de6f75b805eee5cbb3
Thales <thales.souz@outlook.com>
2023-04-12 08:23:27 -0300

    feat: prisma schema correct relation field names

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PrismaService } from './database/prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
