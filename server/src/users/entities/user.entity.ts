8bd8ec7c43a0e93be9ef19641102d99f37a6ea0a
Thales <thales.souz@outlook.com>
2023-04-13 20:58:54 -0300

    refact: change entity to user schema

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,9 +1,31 @@
-export class UserEntity {
-  id: string;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+enum Role {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
   firstName: string;
+
+  @Prop({ type: String, required: true })
   lastName: string;
+
+  @Prop({ type: String, required: true })
   email: string;
+
+  @Prop({ type: String, required: true })
   password: string;
+
+  @Prop({ type: Role, required: false })
   role: Role;
+
+  @Prop({ type: String, required: true })
   phoneNumber: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
