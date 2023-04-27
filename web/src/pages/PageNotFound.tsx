9ccb75fc1164858fc485ca5846cd156d8774f021
Thales <thales.souz@outlook.com>
2023-04-27 16:50:51 -0300

    feat: not found page

--- web/src/pages/PageNotFound.tsx
+++ web/src/pages/PageNotFound.tsx
@@ -8,8 +8,9 @@ function PageNotFound (): JSX.Element {
     <div>
       <Header />
       <Slider />
-      <div className='text-center p-10'>
-        <h1 className='text-6xl font-ace-sc uppercase'>404 - Página não encontrada!</h1>
+      <div className='text-center p-10 text-6xl font-ace-sc uppercase'>
+        <div className='mb-8'>404</div>
+        <h1>Página não encontrada!</h1>
       </div>
       <Footer />
     </div>
