d4c375dccff0ea09ac957d3e3cdce367ac6b02de
Thales <thales.souz@outlook.com>
2023-05-04 18:50:26 -0300

    build: query client provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { queryClient } from './api/queryClient'
+import { QueryClientProvider } from 'react-query'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 )
