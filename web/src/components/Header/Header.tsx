0651863ede4d4fdd7e4a90a6608d91de3c0073fa
Thales <thales.souz@outlook.com>
2023-05-11 13:11:27 -0300

    build: change components directory

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import Dropdown from './Header/Dropdown'
+import Dropdown from './Dropdown'
 
 function Header (): JSX.Element {
   const navigate = useNavigate()
