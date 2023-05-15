70bf5673048b4b5fd9d0f2d48b4b5824b37e1da7
Thales <thales.souz@outlook.com>
2023-05-15 16:11:13 -0300

    fix: vercel.json

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -43,13 +43,13 @@ function Header (): JSX.Element {
     <header
       className={`${
         !headerBlack ? 'bg-transparent' : 'bg-slate-900'
-      } fixed z-10 flex max-h-40 w-full flex-col bg-gradient-to-b from-black to-transparent font-bold text-slate-100 hover:bg-slate-900`}
+      } fixed z-10 flex max-h-40 w-full flex-col bg-gradient-to-b max-sm:max-h-30 from-black to-transparent font-bold text-slate-100 hover:bg-slate-900`}
     >
-      <section className="mt-3 flex justify-between font-semibold">
+      <section className="mt-3 flex justify-between font-semibold h-fit max-sm:h-20 max-md:h-20">
         <nav className="invisible mt-7 ml-2 w-0 font-thin md:visible md:w-44">
           <span>Desenvolvido por Thales Chagas</span>
         </nav>
-        <Link to="/" className="mt-7 h-fit pb-2 font-ace-sc tracking-widest max-sm:text-2xl text-4xl">
+        <Link to="/" className="mt-7 font-ace-sc tracking-widest max-sm:text-2xl text-4xl">
           MAKEPY
         </Link>
         <nav className="mr-3 mt-7 flex justify-center gap-4 text-center max-sm:text-base md:w-44">
@@ -61,7 +61,7 @@ function Header (): JSX.Element {
             </Link>
         </nav>
       </section>
-      <nav className="mx-auto flex gap-2 pb-2 text-base font-semibold text-slate-100">
+      <nav className="mx-auto flex gap-2 pb-3 text-base font-semibold text-slate-100">
         <a className="border-r border-slate-100 pr-2 hover:cursor-pointer">
           PELE
         </a>
