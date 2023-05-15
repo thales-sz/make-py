3e79d8ed986fb9b3f9f4737886a07bd980f7f3c1
Thales <thales.souz@outlook.com>
2023-05-15 17:58:38 -0300

    style: change image banner

--- web/src/components/Banner.tsx
+++ web/src/components/Banner.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Banner (): JSX.Element {
   return (
-    <div className='flex flex-col bg-banner-img h-[400px] bg-center w-2/3 mx-auto mt-20 items-center text-center p-20 gap-2 bg-cover rounded-md shadow-sm'>
-      <h1 className='text-slate-50 font-bold text-4xl drop-shadow-xl max-sm:text-2xl'>Toda nossa linha de maquiagem</h1>
-      <p className='text-slate-50 font-bold text-4xl drop-shadow-xl max-sm:text-2xl'> com até 20% de desconto!</p>
+    <div className='flex flex-col bg-banner-img h-[400px] bg-center w-2/3 mx-auto mt-20 text-center items-center pt-20 bg-cover rounded-lg shadow-sm justify-between'>
+      <h1 className='text-slate-50 font-bold text-3xl drop-shadow-xl shadow-black max-sm:text-2xl'>Toda nossa linha de makes com até 20% de desconto!</h1>
       <button className='bg-slate-50 p-2 shadow-md font-bold rounded-sm hover:shadow-xl hover:scale-105 transform transition duration-500 mt-12'>Saiba mais</button>
+      <span className='text-xs text-slate-50 self-start bottom-1'>*Promoção válida até durar o estoque</span>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'banner-img': "url('https://github.com/thales-sz/make-py/blob/development/web/src/assets/banner.png')"
+        'banner-img': "url('https://images.unsplash.com/photo-1583241800967-3b926edea1f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
       }
     }
   },
